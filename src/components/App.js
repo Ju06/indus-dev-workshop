8efc68a51eed858659fccda7bf9b04d4103d025c
Julien Février <julien.fevrier@etudiant.univ-lr.fr>
2022-04-08 16:20:56 +0200

    dev

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
+        <h1>App</h1>
         <SearchBar onSubmit={this.onSearchSubmit} />
         <ImageList images={this.state.images} />
       </div>
